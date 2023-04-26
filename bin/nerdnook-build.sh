7710084219eba9c314d5f496fb712300d5b8bd51
Alex Guanipatin <sebas5950@gmail.com>
2023-04-26 16:21:25 -0400

    fixing deployment

--- bin/nerdnook-build.sh
+++ bin/nerdnook-build.sh
@@ -2,11 +2,12 @@
 # exit on error
 set -o errexit
 
-rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
-
 bundle install
-# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
-# bundle exec rake assets:clean
+# clean
+rm -rf public
+# build
+npm install --prefix client && npm run build --prefix client
+# migrate
 bundle exec rake db:migrate
+# postbuild
+cp -a client/build/. public/
